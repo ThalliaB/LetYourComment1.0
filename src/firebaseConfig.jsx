8cdb591943474126e51731593920617b4fbdd213
ThalliaB <thalliabrum60@gmail.com>
2024-03-31 13:16:03 -0300

    Add .env file in gitignore with apiKey

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = initializeApp({
-    apiKey: "AIzaSyCby7tmWVQQwMRZqpioPNx7X5peKmktMUY",
+    apiKey: process.env.apiKey,
     authDomain: "letyourcomment.firebaseapp.com",
     projectId: "letyourcomment",
 });
